d698199a65366297b8539be985efaa27f7a4c3c0
VIGNESH <vigneshvignesh0822@gmail.com>
2024-10-16 13:37:27 +0530

    Update settings.py

--- techysoftware/settings.py
+++ techysoftware/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u@uf0m7l1^*g4nt+43qlwom(33qye+zea6d@0h%@kz&r)8qddw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'vignesh0822.pythonanywhere.com']  
 
 
 
